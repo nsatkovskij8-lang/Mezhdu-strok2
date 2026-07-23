75e9616e6945210e828a3de6108d52e04eed26ad
nsatkovskij8-lang <nsatkovskij8@gmail.com>
2026-07-23 18:38:09 +0200

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        fontFamily: "sans-serif",
+        backgroundColor: "#ffffff",
+      }}
+    >
+      <h1>Между строк</h1>
+    </div>
+  );
+}
